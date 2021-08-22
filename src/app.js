9c4e280a1e9140e70be5fc64cd27a293798a42d5
BobBy <ayiteybobby@gmail.com>
2021-08-22 02:03:58 +0000

    Change config core case ignore to false

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signup, Signin } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
+
+export default function App() {
+  const { user } = useAuthListener();
+
+  console.log(user);
+
+  return (
+    <Router>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+    </Router>
+  );
+}
